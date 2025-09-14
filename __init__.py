722920726a65546d9dfee87ebfab23546da00c36
flexyyyapk213 <alexkornev1406@gmail.com>
2025-09-14 22:59:14 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 __name__ = 'ModuFlex'
-__version__ = '0.0.9b1'
+__version__ = '0.0.9b2'
 __author__ = 'flexyyyapk'
 __link__ = 'https://t.me/oprosmenya'
 __gitHub__ = 'https://github.com/flexyyyapk213/ModuFlex'
@@ -16,4 +16,5 @@ __news__ = """
 ```
 """
 __python__ = '>=3.8'
+
 __modules__ = ['pyrogram', 'terminaltexteffects', 'requests', 'platform', 'pyfiglet', 'alive_progress', 'packaging', 'tgcrypto']
